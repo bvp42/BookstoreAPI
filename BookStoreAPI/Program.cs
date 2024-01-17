89abbbc44b6d10dd8d1bccb95bdf3ac7e4cfb232
rraya <rraya@bluepeople.com>
2024-01-17 00:07:07 -0600

    Archivos de configuracion de la base de datos

--- BookStoreAPI/Program.cs
+++ BookStoreAPI/Program.cs
@@ -1,9 +1,13 @@
+using BookStoreAPI.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.Configure<BookStoreDatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
 
 var app = builder.Build();
 
