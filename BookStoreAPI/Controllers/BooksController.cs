9792c2dc395accfc597809b0b39978d2f76f9eba
rraya <rraya@bluepeople.com>
2024-01-17 00:34:04 -0600

    Creacion del controlador para el servicio de libros

---
+++ BookStoreAPI/Controllers/BooksController.cs
@@ -0,0 +1,77 @@
+﻿using BookStoreAPI.Models;
+using BookStoreAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BooksController : ControllerBase
+{
+    private readonly BooksService _booksService;
+
+    public BooksController(BooksService booksService) =>
+        _booksService = booksService;
+    //Get All
+    [HttpGet]
+    public async Task<List<Book>> Get() =>
+        await _booksService.GetAsync();
+    
+    //Get by id
+    [HttpGet("{id:length(24)}")]
+    public async Task<ActionResult<Book>> Get(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        return book;
+    }
+    
+    //Post
+    [HttpPost]
+    public async Task<IActionResult> Post(Book newBook)
+    {
+        await _booksService.CreateAsync(newBook);
+
+        return CreatedAtAction(nameof(Get), new { id = newBook.Id }, newBook);
+    }
+    
+    //Put
+    [HttpPut("{id:length(24)}")]
+    public async Task<IActionResult> Update(string id, Book updatedBook)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        updatedBook.Id = book.Id;
+
+        await _booksService.UpdateAsync(id, updatedBook);
+
+        return NoContent();
+    }
+
+    //Delete
+    [HttpDelete("{id:length(24)}")]
+    public async Task<IActionResult> Delete(string id)
+    {
+        var book = await _booksService.GetAsync(id);
+
+        if (book is null)
+        {
+            return NotFound();
+        }
+
+        await _booksService.RemoveAsync(id);
+
+        return NoContent();
+    }
+
+}
